1a6a1a801d63625e1a7df5faad73cac5f986f142
Jumbo810 <jumbo81033334@gmail.com>
2022-09-30 04:35:22 +0000

    Build 1: Libraries Replaced TO androidx. New ONE

--- app/src/main/java/microsoft/com/pwa/MainActivity.java
+++ app/src/main/java/microsoft/com/pwa/MainActivity.java
@@ -4,7 +4,8 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.res.AssetManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+// import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.*;
 
